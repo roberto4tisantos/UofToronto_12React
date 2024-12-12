0537fbd8dd78cbe41465bbbe6cb47d4846733cbd
FORTIS Roberto <roberto.4ti@gmail.com>
2024-12-12 18:35:39 -0500

    comp modified

--- my-portfolio/src/components/pages/Portfolio.jsx
+++ my-portfolio/src/components/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-undef */
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-//import './Portfolio.css'
+import './Portfolio.css'; // Ensure the CSS file is imported
 
 //Invoke Portfolio 
 const Portfolio = () => {
@@ -9,9 +9,16 @@ const Portfolio = () => {
     <section id="portfolio">
       <h2>Portfolio</h2>
       <p>Are you smarter than a fifth grader?</p>       
-      <img src="/path-to-your-photo.jpg" alt="UofT" />      
-      <link href="https://shumanji7.github.io/Project-1-Best/"></link> 
-      <link href="https://github.com/shumanji7/Project-1-Best.git"></link> 
+      <img src="/path-to-your-photo.jpg" alt="UofT" />
+
+      <div className="links">
+        <a href="https://shumanji7.github.io/Project-1-Best/" target="_blank" rel="noopener noreferrer">
+          View Project 1 (Live)
+        </a>
+        <a href="https://github.com/shumanji7/Project-1-Best.git" target="_blank" rel="noopener noreferrer">
+          View Project 1 (Code)
+        </a>
+      </div>
       {/* Add more projects as needed */}
     </section>
   );
