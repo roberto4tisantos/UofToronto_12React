7a778e5fff23e88b1ee4a425ea39540411695552
FORTIS Roberto <roberto.4ti@gmail.com>
2024-12-12 19:36:35 -0500

    update

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,6 +1,5 @@
 //import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 // import './index.css'
 import './components/style.css'
 import App from './App.jsx'
